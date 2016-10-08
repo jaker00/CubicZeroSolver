a3fe1821d41d9c9f9e446e4f4552923f577ff73a
jaker00 <jfreisner10@gmail.com>
2016-10-07 19:55:45 -0500

    just integrated rational roots theorem

--- CubicZeroSolver.java
+++ CubicZeroSolver.java
@@ -6,22 +6,28 @@ import java.util.*;
 
 public class CubicZeroSolver {
 
-    public static double[] cubic(double a, double b, double c, double d) {
+    public static double[] lead_factorization(double a) {
         int n = (int) a;
-        int m = (int) d;
-
-
-        double[] lead = new double[n];
-        double[] end = new double[m];
-
-        int i, j = 0; //create an array for lead coefficient
-        for (i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                lead[j] = i;
-                j++;
+        double[] lead = new double[100];
+        int i,j = 0;
+        if (a<0){
+            for (i = -1; i >= n; i--) {
+                if (n % i == 0) {
+                    lead[j] = i;
+                    lead[j+1] = -1*i;
+                    j = j+2;
+                }
             }
         }
-
+        else {
+            for (i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    lead[j] = i;
+                    lead[j+1] = -1*i;
+                    j = j+2;
+                }
+            }
+        }
         //erase all values in the lead array equal to 0 (extra array values)
         j = 0;
         for(i=0;  i<lead.length;  i++ ) {
@@ -31,13 +37,31 @@ public class CubicZeroSolver {
         double [] newLead = new double[j];
         System.arraycopy( lead, 0, newLead, 0, j );
 
+        return newLead;
 
-         //create an array for the end term
-        j = 0;
-        for (i = 1; i <= m; i++) {
-            if (m % i == 0) {
-                end[j] = i;
-                j++;
+    }
+
+    public static double[] end_factorization(double d) {
+        int m = (int) d;
+        double[] end = new double[100];
+        //create an array for the end term
+        int i,j = 0;
+        if (d<0){
+            for (i = -1; i >= m; i--) {
+                if (m % i == 0) {
+                    end[j] = i;
+                    end[j+1] = -1*i;
+                    j = j+2;
+                }
+            }
+        }
+        else {
+            for (i = 1; i <= m; i++) {
+                if (m % i == 0) {
+                    end[j] = i;
+                    end[j+1] = -1*i;
+                    j = j+2;
+                }
             }
         }
         //erase all values in the end array equal to 0 (extra array values)
@@ -47,25 +71,36 @@ public class CubicZeroSolver {
             if (end[i] != 0)
                 end[j++] = end[i];
         }
-        double [] newEnd = new double[j];
+        double[] newEnd = new double[j];
         System.arraycopy( end, 0, newEnd, 0, j );
         //return newEnd;
-        return newLead;
+        return newEnd;
 
     }
 
-    //old method, deprecated:
-    //double p = (-1*b)/(3*a);
-    //double q = Math.pow(p,3) + (b*c-3*a*d)/(6*a*a);
-    //double r = c/(3*a);
-    //return Math.pow(q + Math.pow(q*q + Math.pow(r-p*p,3.0),.5),(1/3)) +
-    //Math.pow(q - Math.pow(q*q + Math.pow(r-p*p,3),.5),(1/3)) + p;
+    public static double[] rational_roots_test(double[] newLead, double[] newEnd, double a, double b, double c, double d){
+
+        double[] roots = new double[10];
+        int i,k,l = 0;
+        for(i=0; i<=newEnd.length-1; i++)
+        {
+            for(k=0; k<=newLead.length-1; k++)
+                if (a * Math.pow((newEnd[i] / newLead[k]), 3) + b * Math.pow((newEnd[i] / newLead[k]), 2) + c * (newEnd[i] / newLead[k]) + d == 0) {
+                    roots[l] = newEnd[i] / newLead[k];
+                    l++;
+                }
+        }
+        return roots;
+    }
+
+
+
 
 
 //    public static int[] synth(double a, double b, double c, double d){
-//        int root1 = cubic(a,b,c,d);
-//        //double remainder = d-(root*(c-(root*(b-root*a))));
-//        int[] syn = {a,b-root1*a,c-(b-(root1*a))};
+//        double[] root = rational_roots_test(lead_factorization(a),end_factorization(d),a,b,c,d);
+//        double remainder = d-(root*(c-(root*(b-root*a))));
+//        int[] syn = {a,b-root*a,c-(b-(root*a))};
 //        return syn;
 //    }
 
@@ -98,7 +133,9 @@ public class CubicZeroSolver {
 //            System.out.println("Your solution is: (x-" + result + ")(" + synresult[0] + "x^2  + " + synresult[1] + "x + " + synresult[2] + ")");
 //        else
 //            System.out.println("Your solution is: (x-" + result + ")"+"(x- " + roots[0]+ ")(x-" +roots[1]+")");
-        double[] result = cubic(a, b, c, d);
+
+
+        double[] result = rational_roots_test(lead_factorization(a),lead_factorization(b),a,b,c,d);
         for (int i = 0; i < result.length; i++) {
             System.out.print(" " + result[i]+ " ");
         }
